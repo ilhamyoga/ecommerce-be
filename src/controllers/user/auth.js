b7f1f9f5e02d0903deae4bc5efa473cd0bcc5712
ilhamygp <ilhamyogha@gmail.com>
2024-03-07 15:53:09 +0700

    feat: Add route user register

--- src/controllers/user/auth.js
+++ src/controllers/user/auth.js
@@ -2,10 +2,21 @@
 
 const catchAsync = require('../../utils/catchAsync');
 const response = require('../../utils/response');
+const { Users } = require('../../db/mysql/models');
 
 module.exports = {
   register: catchAsync(async (req, res) => {
-    res.status(200).json(response({ message: 'Register successful' }));
+    const { name, email, password } = req.body;
+
+    const existingEmail = await Users.scope('all').findOne({ where: { email } })
+    if (existingEmail) {
+      return res.status(400).json(response({ error: 'Email already registered' }))
+    }
+
+    const user = await Users.create({ userCode: true, name, email, password });
+    await user.reload();
+
+    res.status(201).json(response({ data: user }));
   }),
 
   login: catchAsync(async (req, res) => {
--- src/routes/user.js
+++ src/routes/user.js
@@ -8,6 +8,7 @@ const authController = require('../controllers/user/auth');
 // AUTH ROUTES
 const authRouter = express.Router()
 authRouter.post('/login', authController.login);
+authRouter.post('/register', authController.register);
 
 module.exports = {
   authRouter
